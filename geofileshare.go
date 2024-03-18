2dad89171c74a61e876e4a6b4bf2c445db08bfaf
Nikos Steiakakis <nikos@geosysta.com>
2024-03-18 17:21:27 +0200

    Added bare template usage

--- geofileshare.go
+++ geofileshare.go
@@ -2,13 +2,52 @@ package main
 
 import (
     "fmt"
+    "html/template"
     "net/http"
 )
 
+type PageData struct {
+    Title string
+    Greeting string
+}
+
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, I see you are vistiting the page on %v\n", r.URL.Path)
+    router := http.NewServeMux()
+    tmpl := make(map[string]*template.Template)
+
+    router.HandleFunc("GET /greeting", func(w http.ResponseWriter, r *http.Request) {
+        tmpl["greeting.html"] = template.Must(template.ParseFiles("templates/greeting.html", "templates/_base.html"))
+
+        data := PageData {
+            Title: "Welcome to Geofileshare",
+            Greeting: fmt.Sprintf("Hello, I see you are vistiting the page on %v\n", r.URL.Path),
+        }
+
+        tmpl["greeting.html"].ExecuteTemplate(w, "base", data)
     })
 
-    http.ListenAndServe(":85", nil)
+    router.HandleFunc("POST /greeting", func(w http.ResponseWriter, r *http.Request) {
+        username := r.FormValue("username")
+
+        data := PageData {
+            Title: "Welcome to Geofileshare",
+            Greeting: fmt.Sprintf("Hello, %v, good to see you can post here", username),
+        }
+        tmpl["greeting.html"].ExecuteTemplate(w, "base", data)
+    })
+
+    router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+        data := PageData {
+            Title: "Welcome to Geofileshare",
+            Greeting: "This is home... ...page :)",
+        }
+        tmpl["index.html"] = template.Must(template.ParseFiles("templates/index.html", "templates/_base.html"))
+        tmpl["index.html"].ExecuteTemplate(w, "base", data)
+    })
+
+
+    fs := http.FileServer(http.Dir("static/"))
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+    http.ListenAndServe(":85", router)
 }
